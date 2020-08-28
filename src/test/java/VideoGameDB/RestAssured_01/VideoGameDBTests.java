bffadca7e7e0f3347748c128c47d161e5ca8a597
nskarthi <nskarthi@gmail.com>
2020-08-28 08:21:08 -0400

    added few more tests

--- src/test/java/VideoGameDB/RestAssured_01/VideoGameDBTests.java
+++ src/test/java/VideoGameDB/RestAssured_01/VideoGameDBTests.java
@@ -13,8 +13,8 @@ public class VideoGameDBTests extends VideoGameConfig {
 	@Test
 	public void getAllGames()  {
 		given().
-		when().
-			get(VideoGamesEndpoints.ALL_VIDEO_GAMES).
+		when()
+			.get(VideoGamesEndpoints.ALL_VIDEO_GAMES).
 		then();
 	}
 
@@ -28,10 +28,51 @@ public class VideoGameDBTests extends VideoGameConfig {
 					"\"category\": \"Tricky\",\n" + 
 					"\"rating\": \"Universal\"\n" + 
 				"}";
-		given().
-			body(gameBodyJson).
-		when().
-			post(VideoGamesEndpoints.ALL_VIDEO_GAMES).
+		given()
+			.body(gameBodyJson).
+		when()
+			.post(VideoGamesEndpoints.ALL_VIDEO_GAMES).
 		then();
 	}
+
+	@Test
+	public void createNewGameByXML()  {
+		String gameBodyXML = "<videoGame category=\"Tricky\" rating=\"Universal\">" + 
+					"<id>101</id>\n" + "<name>Super Mario 5</name>\n" + 
+					"<releaseDate>2019-12-01T00:00:00Z</releaseDate>\n" + 
+					"<reviewScore>90</reviewScore>\n" + "</videoGame>";
+	given()
+		.body(gameBodyXML)
+		.header("Content-Type", "application/xml")
+		.header("Accept", "application/xml").
+	when()
+		.post(VideoGamesEndpoints.ALL_VIDEO_GAMES).
+	then();
+	}
+
+	@Test
+	public void updateGameByJSON()  {
+		String gameBodyJson = "{\n" + 
+					"\"id\": 100,\n" +
+					"\"name\": \"Super Mario 4\",\n" +
+					"\"releaseDate\": \"2019-10-01\",\n" + 
+					"\"reviewScore\": 91,\n" + 
+					"\"category\": \"Tricky\",\n" + 
+					"\"rating\": \"Universal\"\n" + 
+				"}";
+		given()
+			.body(gameBodyJson).
+		when()
+			.put("videogames/100").
+		then();
+	}
+
+	@Test
+	public void deleteGame()  {
+		given().
+		when()
+			.delete("videogames/101").
+		then();
+	}
+	
 }
