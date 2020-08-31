cd8df4c31f665faa1261401b5e2d9b4aa8d8a639
nskarthi <nskarthi@gmail.com>
2020-08-31 13:07:43 -0700

    Renamed file and added football config details
    Renamed file and added football config details

--- src/test/java/VideoGameDB/config/VideoGamesEndpoints.java
+++ src/test/java/VideoGameDB/config/VideoGamesEndpoints.java
@@ -1,6 +1,10 @@
 package VideoGameDB.config;
 
-public interface VideoGamesEndpoints {
+public interface Endpoints {
 	String ALL_VIDEO_GAMES = "videogames";
 	String SINGLE_VIDEO_GAME = "videogames/{videoGameId}";
+	
+	String TEAM_DETAILS = "teams/57";
+	String COMPETITION_DETAILS = "competitions/2021/teams";
+	
 }
